03c071544f44589b09129ec75d3171c63d04fa79
omesh4181 <omesh.ch0151@gmail.com>
2025-03-27 00:46:30 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,9 +8,9 @@ import img9 from './images2.jpeg';
 import img10 from './images3.jpg';
 import img11 from './front-left.avif';
 import img12 from './Honda-CR-V-2021.avif';
-import img13 from './5-series-vs-E-class.jpg';
+// import img13 from './5-series-vs-E-class.jpg';
 import img14 from './car1.jpeg';
 import vid1 from './videoplayback.mp4';
 import vid2 from './video2.mp4';
 
-export { img4,img5,img6,img8,img9,img10,img11,img12,img13,img14,vid1,vid2 };
+export { img4,img5,img6,img8,img9,img10,img11,img12,img14,vid1,vid2 };
